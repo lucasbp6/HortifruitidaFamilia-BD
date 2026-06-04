1088c6bbbfa98b97b3b1586673c2a5c9594d2c4a
lucasbp6 <lucasbatistapereira06@gmail.com>
2026-06-04 15:29:09 -0300

    estilo

--- crud/select.py
+++
@@ -1,9 +0,0 @@
-# nao  feito
-
-
-def select(query: str):
-    with get_connection() as conn:
-        with conn.cursor() as cur:
-            cur.execute(query)
-
-            return cur.fetchall()
